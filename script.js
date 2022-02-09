1d494dfddd6f32b286c92dfd089bd2781bb8365d
vicvitoria666 <https.vitoriaoliveira@gmail.com>
2022-02-09 17:33:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let name = 'Vitória'
+
+let age = 20
+
+let year = 2022 - age
+console.log ( year + age )
+
+console.log('estou mexendo no console.log')
+
+let quartaFeira = false;
+console.log (`they told me today is Wednesday and it's`)
+console.log(`a let quartaFeira é ${typeof quartaFeira}`)
